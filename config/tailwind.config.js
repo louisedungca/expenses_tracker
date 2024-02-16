e83d45c30cd468a16bc33b83c0c2601a12424373
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-16 13:36:41 +0800

    style: add base styling for form view

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,11 +10,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["'Inter var'", ...defaultTheme.fontFamily.sans],
+        serif: ['Rubik', ...defaultTheme.fontFamily.serif],
+        sans: ['Space Mono', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
         "retro__dark": "5px 5px 0px 0px rgba(28, 25, 23, 0.85)",
-      }
+        "retro__circle": "3px 3px 0px 0px rgba(28, 25, 23, 0.85)",
+      },
+      fontSize: {
+        fSize__xl: "clamp(2.5rem, 1.2389rem + 6.7257vw, 8rem)",
+        fSize__lg: "clamp(0.75rem, 0.6903rem + 0.3186vw, 1.2rem)",
+        fSize__sm: "clamp(0.7rem, 0.6735rem + 0.1416vw, 0.9rem)",
+      },
     },
   },
   plugins: [
