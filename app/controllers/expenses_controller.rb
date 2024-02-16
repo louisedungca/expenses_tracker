efdb2bf6a5875d88bd397fd8ff8b9ed0991c1250
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-16 23:11:33 +0800

    chore: remove unused files

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,5 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, except: [:index, :new, :create, :monthly_expenses]
+  before_action :set_expense, except: [:index, :new, :create, :show_monthly_expenses]
 
   def index
     @expenses = Expense.all.order(date: :desc)
@@ -8,6 +8,14 @@ class ExpensesController < ApplicationController
 
   def show
     redirect_to expenses_month_path(@expense.date.beginning_of_month)
+  end
+
+  def show_monthly_expenses
+    month_start = params[:month].to_date.beginning_of_month
+    month_end = month_start.end_of_month
+
+    @expenses = Expense.where(date: month_start..month_end).order(date: :desc)
+    @total_amount = @expenses.sum(:amount)
   end
 
   def new
@@ -46,14 +54,6 @@ class ExpensesController < ApplicationController
       flash[:alert] = "Oops, a problem occurred. Please try again."
       render :index, status: 422
     end
-  end
-
-  def monthly_expenses
-    month_beginning = params[:month].to_date.beginning_of_month
-    month_end = month_beginning.end_of_month
-
-    @expenses = Expense.where(date: month_beginning..month_end).order(date: :desc)
-    @total_amount = @expenses.sum(:amount)
   end
 
   private
